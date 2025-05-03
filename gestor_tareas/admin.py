ac765f22049f3704b0c3701140a5c963bda35abb
MontesTapiero <montestapiero@gmail.com>
2025-05-03 13:57:04 -0500

    5

--- gestor_tareas/admin.py
+++ gestor_tareas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import gestor_tareas
+from .models import Task
 
 # Register your models here.
-admin.site.register(gestor_tareas)
+admin.site.register(Task)
