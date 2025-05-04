8ee204e59012387f8583fa7b23bf562c6a080c4e
MontesTapiero <montestapiero@gmail.com>
2025-05-04 17:39:25 -0500

    8

--- gestor_tareas/urls.py
+++ gestor_tareas/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from gestor_tareas import views
 
@@ -6,5 +7,6 @@ router = routers.DefaultRouter()
 router.register(r'gestor_tareas', views.TaskView, 'gestor_tareas')
 
 urlpatterns = [
-    path("api/v1/", include(router.urls))
+    path("api/v1/", include(router.urls)) ,
+    path('docs/', include_docs_urls(title="Gestor Tareas API"))
 ]
